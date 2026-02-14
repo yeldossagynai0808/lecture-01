04bed01cc248b8239ec5c69560e1471693677fab
yeldossagynai0808 <yeldos.sagynai@narxoz.kz>
2026-02-14 09:46:19 +0500

    добавлен txt

--- lab-01/2.py
+++ lab-01/2.py
@@ -1,11 +1,26 @@
 def analyse(text):
-    count = 0
     vowels = 'aeiouy'
+    unique_vowels = []
+    found_words = []
+    current_word = ""
 
-    for ch in text.lower():
-        if ch in vowels:
-            count += 1
+    text = text.lower
 
-    return count
+    for ch in text:
+        if ch in vowels and ch not in unique_vowels:
+            unique_vowels.append(ch)
 
-print(analyse('Hello'))
+        if ch.isalpha():
+            current_word += ch
+        else:
+            if len(current_word) >= 5:
+                if current_word[0] == current_word[-1]:
+                    found_word.append(current_word)
+            current_word = ""
+    if len(current_word) >= 5:
+        if current_word[0] == current_word[-1]:
+            if current_word not in found_words:
+                found_words.append(current_word)
+    return(len(unique_vowels), " ".join(fouund_word))
+
+print(analyse('Level madam Anna went to kayak racecar!'))
